427050435daedb8de9793671e8cbf15cbef54689
docvinum <21454833-docvinum@users.noreply.replit.com>
2023-04-23 19:20:01 +0000

    plugin header

--- crossref-doi-deposit.php
+++ crossref-doi-deposit.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/docvinum/Wordpress-Crossref-DOI-deposit-plugin
 Plugin Author: DocVinum
 Plugin Version: 0.1.0
 Description: A WordPress plugin to deposit DOIs to Crossref for published content.
-Author: DocVinum
+Author: DocVinum @ EtOH
 Author URI: https://github.com/docvinum
 License: MIT
 Text Domain: crossref-doi-deposit
